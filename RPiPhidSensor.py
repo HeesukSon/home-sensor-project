7bfd1f00ce5a3172c87f132418537b9089e26537
Heesuk Son <heesuk.chad.son@gmail.com>
2020-04-18 16:17:17 -0400

    code structure changed

--- RPiPhidSensor.py
+++ RPiPhidSensor.py
@@ -4,6 +4,7 @@ from Phidget22.Devices.VoltageInput import *
 import termplotlib as tpl
 import numpy as np
 import time
+import json
 
 hub_sn = 540054
 sound1_port = 0
@@ -16,24 +17,41 @@ waitT = 10000
 
 def main():
         snd1 = SoundSensor()
+        snd2 = SoundSensor()
+        temp = VoltageRatioInput()
+        hum = VoltageRatioInput()
+        light = VoltageInput()
+
+        openChannels(snd1, snd2, temp, hum, light)
+
+        for i in range(10):
+                print("Sound1 Level: {} dB".format(snd1.getdB()))
+                print("Sound2 Level: {} dB".format(snd2.getdB()))
+                print("Temperature: {}".format(temp.getSensorValue()))
+                print("Humidity: {}".format(hum.getSensorValue()))
+                print("Precision Light: {}".format(light.getSensorValue()))
+                # uncomment this line and the one below to visualize octaves values
+                # plotOct(snd1.getOctaves())
+                print('')
+                time.sleep(1)
+
+        snd1.close()
+
+def openChannels(snd1, snd2, temp, hum, light):
         snd1.setDeviceSerialNumber(hub_sn)
         snd1.setHubPort(sound1_port)
 
-        snd2 = SoundSensor()
         snd2.setDeviceSerialNumber(hub_sn)
         snd2.setHubPort(sound2_port)
 
-        temp = VoltageRatioInput()
         temp.setDeviceSerialNumber(hub_sn)
         temp.setHubPort(temp_port)
         temp.setIsHubPortDevice(True)
 
-        hum = VoltageRatioInput()
         hum.setDeviceSerialNumber(hub_sn)
         hum.setHubPort(hum_port)
         hum.setIsHubPortDevice(True)
 
-        light = VoltageInput()
         light.setDeviceSerialNumber(hub_sn)
         light.setHubPort(light_port)
         light.setIsHubPortDevice(True)
@@ -48,17 +66,6 @@ def main():
         hum.setSensorType(VoltageRatioSensorType.SENSOR_TYPE_1125_HUMIDITY)
         light.setSensorType(VoltageSensorType.SENSOR_TYPE_1127)
 
-        for i in range(10):
-                print("Sound1 Level: {} dB".format(snd1.getdB()))
-                print("Sound2 Level: {} dB".format(snd2.getdB()))
-                print("Temperature: {}".format(temp.getSensorValue()))
-                print("Humidity: {}".format(hum.getSensorValue()))
-                print("Precision Light: {}".format(light.getSensorValue()))
-                #plotOct(snd1.getOctaves())
-                time.sleep(1)
-
-        snd1.close()
-
 def plotOct(octaves):
         oct_range = np.array(['31.5', '63', '125', '250', '500', '1k', '2k', '4k', '8k', '16k'])
         oct_values = np.array([round(x) for x in octaves])
@@ -68,5 +75,4 @@ def plotOct(octaves):
         fig.show()
         print('')
 
-# call main()
 main()
