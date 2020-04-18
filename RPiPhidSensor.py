6b714012e35333bcb5a3abb6936bb2d6f52cc737
Heesuk Son <heesuk.chad.son@gmail.com>
2020-04-18 15:11:40 -0400

    Sound2 added

--- RPiPhidSensor.py
+++ RPiPhidSensor.py
@@ -3,19 +3,31 @@ import termplotlib as tpl
 import numpy as np
 import time
 
-def main():
-        ch = SoundSensor()
-        ch.setDeviceSerialNumber(540054)
-        ch.setHubPort(0)
+hub_sn = 540054
+sound1_port = 0
+sound2_port = 1
+light_port = 5
+temp_port = 4
+hum_port = 3
 
-        ch.openWaitForAttachment(5000)
+def main():
+        snd1 = SoundSensor()
+        snd1.setDeviceSerialNumber(hub_sn)
+        snd1.setHubPort(sound1_port)
+
+        snd2 = SoundSensor()
+        snd2.setDeviceSerialNumber(hub_sn)
+        snd2.setHubPort(sound2_port)
+
+        snd1.openWaitForAttachment(5000)
 
         for i in range(10):
-                print("Sound Level: {} dB".format(ch.getdB()))
-                plot(ch.getOctaves())
+                print("Sound1 Level: {} dB".format(snd1.getdB()))
+                print("Sound2 Level: {} dB".format(snd2.getdB()))
+                #plot(snd1.getOctaves())
                 time.sleep(1)
 
-        ch.close()
+        snd1.close()
 
 def plot(octaves):
         oct_range = np.array(['31.5', '63', '125', '250', '500', '1k', '2k', '4k', '8k', '16k'])
