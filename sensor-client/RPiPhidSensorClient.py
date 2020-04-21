82beea22602de984edd4a423bc9c133ff658538b
Heesuk Son <heesuk.chad.son@gmail.com>
2020-04-21 17:49:29 -0400

    year to include century

--- sensor-client/RPiPhidSensorClient.py
+++ sensor-client/RPiPhidSensorClient.py
@@ -69,7 +69,7 @@ def getJSONSensorValues(snd1, snd2, temp, hum, light):
         data = {
                 "From":getIPAddress(),
                 #"At":time.ctime(time.time()),
-                "At":time.strftime("%y-%m-%d %H:%M:%S", time.localtime()),
+                "At":time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
                 "Sound1":snd1.getOctaves(),
                 "Sound2":snd2.getOctaves(),
                 "Temperature":temp.getSensorValue(),
