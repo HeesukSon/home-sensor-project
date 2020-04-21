b5bb53ab37c55210c6e8da116030746fdc12889d
Heesuk Son <heesuk.chad.son@gmail.com>
2020-04-21 17:46:55 -0400

    client also edited

--- sensor-client/RPiPhidSensorClient.py
+++ sensor-client/RPiPhidSensorClient.py
@@ -68,7 +68,8 @@ def main():
 def getJSONSensorValues(snd1, snd2, temp, hum, light):
         data = {
                 "From":getIPAddress(),
-                "At":time.ctime(time.time()),
+                #"At":time.ctime(time.time()),
+                "At":time.strftime("%y-%m-%d %H:%M:%S", time.localtime()),
                 "Sound1":snd1.getOctaves(),
                 "Sound2":snd2.getOctaves(),
                 "Temperature":temp.getSensorValue(),
