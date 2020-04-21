82b19a5d1d808b340c740b51a7a33d96d47b0848
Heesuk Son <heesuk.chad.son@gmail.com>
2020-04-20 16:00:41 -0900

    local-print

--- sensor-client/RPiPhidSensor.py
+++ sensor-client/RPiPhidSensor.py
@@ -51,9 +51,9 @@ def getJSONSensorValues(snd1, snd2, temp, hum, light):
         return data
 
 def getIPAddress():
-        return str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] 
-        if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), 
-        s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) 
+        return str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+        if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
+        s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]])
         if l][0][0])
 
 def openChannels(snd1, snd2, temp, hum, light):
