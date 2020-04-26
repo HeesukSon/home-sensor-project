64a0b436fe8d97dc2a9893f8eea6f4f33ad3f6b5
Heesuk Son <heesuk.chad.son@gmail.com>
2020-04-26 10:10:07 -0400

    Try catch edited to prevent stops due to connection error

--- sensor-client/RPiPhidSensorClient.py
+++ sensor-client/RPiPhidSensorClient.py
@@ -48,23 +48,23 @@ def main():
         # Create a TCP/IP socket
 
         # send sensed data to db server
-        #for i in range(1000):
-        try:        
-                while(True):
-                        try:
-                                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                server_address = (srv_ip, PORT)
-                                sock.connect(server_address)
-                                time.sleep(0.1)
-                                sensor_data = getJSONSensorValues(snd1, snd2, temp, hum, light, motion)
-                                sock.sendall(pickle.dumps(sensor_data))
-                                print('Sensor data has been sent: {}'.format(sensor_data))
-                        finally:
-                                sock.close()
-                                print('Socket has been closed.')
-                        time.sleep(1)
-        except Exception as e:
-                print("Exception: {}".format(e.details))
+        #for i in range(1000):    
+        while(True):
+                try:
+                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        server_address = (srv_ip, PORT)
+                        sock.connect(server_address)
+                        time.sleep(0.1)
+                        sensor_data = getJSONSensorValues(snd1, snd2, temp, hum, light, motion)
+                        sock.sendall(pickle.dumps(sensor_data))
+                        print('Sensor data has been sent: {}'.format(sensor_data))
+                except Exception as e:
+                        print("Exception found: {}".format(type(e)))
+                finally:
+                        sock.close()
+                        print('Socket has been closed.')
+                time.sleep(1)
+        
 
         # close Phidget channels
         snd1.close()
